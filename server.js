91f79c21daecc8235c80d73efba27bc4516a5693
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-03-05 18:33:17 -0600

    updated routes/htmlroutes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/htmlroutes")(app);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
